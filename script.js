f25e6348eea6ebeb59ef89d64e48b262357d92de
Bhumika Maharjan <bhumika2060@gmail.com>
2024-07-30 19:02:27 +0545

    responsive

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Sticky navbar functionality
     window.addEventListener("scroll", function() {
         if (window.scrollY > 20) {
             document.querySelector('.navbar').classList.add("sticky");
         } else {
             document.querySelector('.navbar').classList.remove("sticky");
         }
+    });
+
+    // Toggle menu on click
+    $('.menu-btn').on('click', function() {
+        $('.navbar .menu').toggleClass('active');
+        $('.menu-btn i').toggleClass('active');
     });
 });
